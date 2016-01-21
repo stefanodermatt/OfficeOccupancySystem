71c27657eca5e0819ab5b24e021c36ae37e44c22
Odermatt, Stefan <Stefan.Odermatt@Phonak.com>
2016-01-21 19:02:11 +0100

    package.json updated

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/dist'));
 
+app.get('/', function(request, response) {
+  response.render('index.html');
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
