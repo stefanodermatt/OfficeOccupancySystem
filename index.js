1e7f96ae0b410140b803781af3f9dc5e9b642555
Odermatt, Stefan <Stefan.Odermatt@Phonak.com>
2016-01-21 11:24:08 +0100

    index.js: view engine set

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/dist'));
 
+// views is directory for all template files
+app.set('views', __dirname + '/dist');
+app.set('view engine', 'ejs');
+
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
