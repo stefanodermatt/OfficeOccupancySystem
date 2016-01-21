4263fbd1e7c91347358f8aff490b564087bdd722
Odermatt, Stefan <Stefan.Odermatt@Phonak.com>
2016-01-21 18:52:16 +0100

    package.json updated

--- index.js
+++ index.js
@@ -5,13 +5,6 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/dist'));
 
-app.set('view engine', 'ejs');
-app.engine('.html', require('ejs').renderFile);
-
-app.get('/', function(request, response) {
-  response.render('index');
-});
-
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
