949800c4ddad83225ccd739391af8e4dad38591e
Odermatt, Stefan <Stefan.Odermatt@Phonak.com>
2016-01-21 19:08:28 +0100

    view mapping added

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/dist'));
 
+app.set('views', __dirname + '/dist');
+
 app.get('/', function(request, response) {
   response.render('index.html');
 });
