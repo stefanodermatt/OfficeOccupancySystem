7ed518c656ab0854aeef9a80885a339772fbbdc3
Odermatt, Stefan <Stefan.Odermatt@Phonak.com>
2016-01-21 19:11:42 +0100

    package.json updated

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/dist'));
 
-app.set('views', __dirname + '/dist');
+app.set('views', '/dist');
 
 app.get('/', function(request, response) {
   response.render('index.html');
